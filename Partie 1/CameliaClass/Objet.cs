49fa2cbb9a753ee3b436fa0a610b922819f56a62
Émilie ROGER <emilie.roger@ensc.fr>
2017-04-05 22:13:03 +0200

    Définition de la classe Objet

--- Partie 1/CameliaClass/Objet.cs
+++ Partie 1/CameliaClass/Objet.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace CameliaClass
 {
-    class Objet
+    public class Objet
     {
+        public int Ligne { get; set; }
+        public int Colonne { get; set; }
+        public int Orientation { get; set; }
+        public int Hauteur { get; set; }
+
+        public Objet(int x, int y, int k, int z)
+        {
+            Ligne = x;
+            Colonne = y;
+            Orientation = k;
+            Hauteur = z;
+        }
+
+        public override string ToString()
+        {
+            return "Objet :\n  - Ligne : " + this.Ligne + "\n  - Colonne : " + this.Colonne + "\n  - Orientation : " + this.Orientation + "\n  - Hauteur : " + this.Hauteur;
+        }
     }
 }
