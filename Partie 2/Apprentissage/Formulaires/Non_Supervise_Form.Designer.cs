a2ac5f16f11490171e833d5306cd4d99dd7e7626
Émilie ROGER <emilie.roger@ensc.fr>
2017-04-06 11:33:03 +0200

    Formulaire pour l’apprentissage non-supervisé

---
+++ Partie 2/Apprentissage/Formulaires/Non_Supervise_Form.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Formulaire
+{
+    partial class Non_Supervise_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Non_Supervise_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 236);
+            this.Name = "Non_Supervise_Form";
+            this.Text = "Apprentissage Non-Supervisé";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
