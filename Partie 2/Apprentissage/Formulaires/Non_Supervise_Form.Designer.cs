b0be8e0ffc4844af6af80ebb94ba800518ef05c7
Émilie ROGER <emilie.roger@ensc.fr>
2017-04-06 23:13:13 +0200

    Formulaire pour l’apprentissage non-supervisé

--- Partie 2/Apprentissage/Formulaires/Non_Supervise_Form.Designer.cs
+++ Partie 2/Apprentissage/Formulaires/Non_Supervise_Form.Designer.cs
@@ -28,19 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Non_Supervise_Form));
+            this.NbLignes_Label = new System.Windows.Forms.Label();
+            this.NbLignes_TextBox = new System.Windows.Forms.TextBox();
+            this.NbColonnes_Label = new System.Windows.Forms.Label();
+            this.NbColonnes_TextBox = new System.Windows.Forms.TextBox();
+            this.CoefApprentissage_Label = new System.Windows.Forms.Label();
+            this.CoefApprentissage_TextBox = new System.Windows.Forms.TextBox();
+            this.Carte_Button = new System.Windows.Forms.Button();
+            this.Resultat_PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Resultat_PictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // NbLignes_Label
+            // 
+            this.NbLignes_Label.Location = new System.Drawing.Point(10, 15);
+            this.NbLignes_Label.Name = "NbLignes_Label";
+            this.NbLignes_Label.Size = new System.Drawing.Size(160, 20);
+            this.NbLignes_Label.TabIndex = 0;
+            this.NbLignes_Label.Text = "Nombres de lignes de la carte :";
+            this.NbLignes_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // NbLignes_TextBox
+            // 
+            this.NbLignes_TextBox.Location = new System.Drawing.Point(170, 15);
+            this.NbLignes_TextBox.Name = "NbLignes_TextBox";
+            this.NbLignes_TextBox.Size = new System.Drawing.Size(50, 20);
+            this.NbLignes_TextBox.TabIndex = 1;
+            this.NbLignes_TextBox.Text = "10";
+            // 
+            // NbColonnes_Label
+            // 
+            this.NbColonnes_Label.Location = new System.Drawing.Point(240, 15);
+            this.NbColonnes_Label.Name = "NbColonnes_Label";
+            this.NbColonnes_Label.Size = new System.Drawing.Size(170, 20);
+            this.NbColonnes_Label.TabIndex = 2;
+            this.NbColonnes_Label.Text = "Nombres de colonnes de la carte :";
+            this.NbColonnes_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // NbColonnes_TextBox
+            // 
+            this.NbColonnes_TextBox.Location = new System.Drawing.Point(410, 15);
+            this.NbColonnes_TextBox.Name = "NbColonnes_TextBox";
+            this.NbColonnes_TextBox.Size = new System.Drawing.Size(50, 20);
+            this.NbColonnes_TextBox.TabIndex = 3;
+            this.NbColonnes_TextBox.Text = "10";
+            // 
+            // CoefApprentissage_Label
+            // 
+            this.CoefApprentissage_Label.Location = new System.Drawing.Point(480, 15);
+            this.CoefApprentissage_Label.Name = "CoefApprentissage_Label";
+            this.CoefApprentissage_Label.Size = new System.Drawing.Size(150, 20);
+            this.CoefApprentissage_Label.TabIndex = 4;
+            this.CoefApprentissage_Label.Text = "Coefficient d’apprentissage :";
+            this.CoefApprentissage_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // CoefApprentissage_TextBox
+            // 
+            this.CoefApprentissage_TextBox.Location = new System.Drawing.Point(630, 15);
+            this.CoefApprentissage_TextBox.Name = "CoefApprentissage_TextBox";
+            this.CoefApprentissage_TextBox.Size = new System.Drawing.Size(50, 20);
+            this.CoefApprentissage_TextBox.TabIndex = 5;
+            this.CoefApprentissage_TextBox.Text = "0,5";
+            // 
+            // Carte_Button
+            // 
+            this.Carte_Button.Location = new System.Drawing.Point(730, 10);
+            this.Carte_Button.Name = "Carte_Button";
+            this.Carte_Button.Size = new System.Drawing.Size(80, 30);
+            this.Carte_Button.TabIndex = 6;
+            this.Carte_Button.Text = "Valider";
+            this.Carte_Button.UseVisualStyleBackColor = true;
+            this.Carte_Button.Click += new System.EventHandler(this.Carte_Button_Click);
+            // 
+            // Resultat_PictureBox
+            // 
+            this.Resultat_PictureBox.Image = ((System.Drawing.Image)(resources.GetObject("Resultat_PictureBox.Image")));
+            this.Resultat_PictureBox.Location = new System.Drawing.Point(10, 50);
+            this.Resultat_PictureBox.Name = "Resultat_PictureBox";
+            this.Resultat_PictureBox.Size = new System.Drawing.Size(800, 800);
+            this.Resultat_PictureBox.TabIndex = 7;
+            this.Resultat_PictureBox.TabStop = false;
             // 
             // Non_Supervise_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(276, 236);
+            this.ClientSize = new System.Drawing.Size(820, 860);
+            this.Controls.Add(this.Resultat_PictureBox);
+            this.Controls.Add(this.NbLignes_Label);
+            this.Controls.Add(this.NbLignes_TextBox);
+            this.Controls.Add(this.Carte_Button);
+            this.Controls.Add(this.NbColonnes_Label);
+            this.Controls.Add(this.NbColonnes_TextBox);
+            this.Controls.Add(this.CoefApprentissage_Label);
+            this.Controls.Add(this.CoefApprentissage_TextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
             this.Name = "Non_Supervise_Form";
-            this.Text = "Apprentissage Non-Supervisé";
+            this.Text = "Apprentissage Supervisé";
+            ((System.ComponentModel.ISupportInitialize)(this.Resultat_PictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NbLignes_Label;
+        private System.Windows.Forms.TextBox NbLignes_TextBox;
+        private System.Windows.Forms.Label NbColonnes_Label;
+        private System.Windows.Forms.TextBox NbColonnes_TextBox;
+        private System.Windows.Forms.Label CoefApprentissage_Label;
+        private System.Windows.Forms.TextBox CoefApprentissage_TextBox;
+        private System.Windows.Forms.Button Carte_Button;
+        private System.Windows.Forms.PictureBox Resultat_PictureBox;
     }
 }
--- Partie 2/Apprentissage/Formulaires/Non_Supervise_Form.cs
+++ Partie 2/Apprentissage/Formulaires/Non_Supervise_Form.cs
@@ -3,17 +3,132 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Classes;
 
 namespace Formulaire
 {
     public partial class Non_Supervise_Form : Form
     {
+        private static List<Observation> Observations = new List<Observation>();
+        private static Random Alea = new Random();
+
+        private int NbLignes;
+        private int NbColonnes;
+        private double CoefApprentissage;
+
+        private Graphics Graphe; // Objet graphique placé en global
+        private Bitmap Image;    // Image figurant la carte
+        private Pen Crayon;      // Crayon placé en global
+
+        private Carte Carte;
+        private static List<Classe> Classes = new List<Classe>();
+
         public Non_Supervise_Form()
         {
             InitializeComponent();
+            Image = (Bitmap)Resultat_PictureBox.Image;
+            Graphe = Graphics.FromImage(Resultat_PictureBox.Image);
+            Crayon = new Pen(Color.White, 2);
+            RecupererDonnees("../../../Donnees/datasetclassif.txt");
+        }
+
+        private void Carte_Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NbLignes = Convert.ToInt32(NbLignes_TextBox.Text);
+                NbColonnes = Convert.ToInt32(NbColonnes_TextBox.Text);
+
+                CoefApprentissage = Convert.ToDouble(CoefApprentissage_TextBox.Text);
+
+                Classes.Clear();
+                Carte = new Carte(NbLignes, NbColonnes, 2, Image.Width);
+                Carte.AlgoKohonen(Observations, CoefApprentissage);
+                Classes = Carte.Regroupement(Observations, 6, Classes);
+
+                Crayon.Color = Color.White;
+                Graphe.FillRectangle(Crayon.Brush, 0, 0, Image.Width, Image.Height);
+                AfficherDonnees();
+                AfficherClasses();
+            }
+
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RecupererDonnees(string fichier_source)
+        {
+            try
+            {
+                System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("iso-8859-1");
+                StreamReader Lecteur = new StreamReader(fichier_source, encoding);
+
+                Observations = new List<Observation>();
+                string Donnee = Lecteur.ReadLine();
+                int Item = 2;
+                List<double> Coordonnees = new List<double> { 0, 0 };
+
+                while (Donnee != null)
+                {
+                    double DonneeChiffree = Convert.ToDouble(Donnee);
+                    if (Item != 2)
+                    {
+                        Coordonnees[Item] = DonneeChiffree;
+                        Item++;
+                    }
+                    else
+                    {
+                        Observations.Add(new Observation(Coordonnees[0], Coordonnees[1]));
+                        Coordonnees[0] = 0;
+                        Coordonnees[1] = 0;
+                        Item = 0;
+                    }
+
+                    Donnee = Lecteur.ReadLine();
+                }
+
+                Observations.RemoveAt(0);
+
+                Lecteur.Close();
+            }
+
+            catch (Exception Ex)
+            {
+                string Erreur = "Une erreur est survenue au cours de l’opération :";
+                MessageBox.Show(Erreur + "\n" + Ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AfficherDonnees()
+        {
+            for (int i = 0; i < Observations.Count; i++)
+            {
+                Image.SetPixel(Convert.ToInt32(Observations[i].Ligne), Convert.ToInt32(Observations[i].Colonne), Color.Black);
+            }
+        }
+
+        private void AfficherClasses()
+        {
+            List<Color> Couleurs = new List<Color> { Color.Red, Color.Blue, Color.Green, Color.Brown, Color.Orange, Color.Purple };
+            for (int i = 0; i < 6; i++)
+            {
+                Crayon.Color = Couleurs[i];
+
+                foreach (Classes.Point point in Classes[i].ListePoints)
+                {
+                    int x = Convert.ToInt32(point.RecupererUnPoids(0));
+                    int y = Convert.ToInt32(point.RecupererUnPoids(1));
+                    Graphe.DrawEllipse(Crayon, x - 2, y - 2, 4, 4);
+                }
+            }
+
+            Resultat_PictureBox.Refresh();
         }
     }
 }
